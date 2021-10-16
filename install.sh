22360c9792e5e699c566ab1cb663707310caabb7
ayamoto <koh14taka2@gmail.com>
2021-10-16 12:34:37 +0900

    create install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,24 @@ has() {
 
 if if [ ! -d {DOT_DIR} ]; then
     if has "git"; then
-        git clone  
+        git clone https://github.com/AyamotoKohei/dotfiles.git ${DOT_DIR}
+    elif has "curl" || has "wget" then
+        TARBALL="https://github.com/AyamotoKohei/dotfiles/archive/refs/heads/main.tar.gz"
+        if has "curl"; then
+            curl -L ${TARBALL} -o main.tar.gz
+        else
+            wget ${TARBALL}
+        fi
+        tar -zxvf main.tar.gz
+        rm -f main.tar.gz
+        mv -f dotfiles-main "${DOT_DIR}"
+    else
+        echo "curl or wget or git required"
+        exit 1
+    fi
+
+    cd ${DOT_DIR}
+
     for f in *;
     do
         [[ "$f" == ".git" ]] && continue
